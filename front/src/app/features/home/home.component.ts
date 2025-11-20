7a27578a73ee6cab2bf845e98fa2de996c252bb2
Samuel Hentrics <samuel.hentrics@gmail.com>
2025-11-20 12:17:15 +0100

    push

--- front/src/app/features/home/home.component.ts
+++ front/src/app/features/home/home.component.ts
@@ -30,7 +30,7 @@ export class HomeComponent {
 
   products: Product[] = [
     { id: 1, name: 'Plateau de fromages fermiers', price: 24.9, category: 'Fromages', 
-      image: 'https://odelices.ouest-france.fr/images/articles/plateau-fromages.jpg', description: 'Assortiment de 4 fromages artisanaux.' },
+      image: 'https://agour.com/2706-large_default/coffret-gipuzkoa.jpg', description: 'Assortiment de 4 fromages artisanaux.' },
     { id: 2, name: 'Saucisson sec traditionnel', price: 12.5, category: 'Saucissons', 
       image: 'https://e4axzwka7un.exactdn.com/wp-content/uploads/2025/09/recette-saucisson-sec-de-campagne.jpg?strip=all&lossy=1&resize=536%2C366&ssl=1', description: 'Saucisson de porc fermier, affiné 6 semaines.' },
     { id: 3, name: 'Pâté de campagne', price: 8.0, category: 'Pâtés', 
